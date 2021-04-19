f3bcd65194507d8012e9c1df97f97cd9e827b8a1
Miguel Solano <miguel.abreu03@gmail.com>
2021-04-19 11:56:45 -0400

    feat: Select elements of the DOM to interact

--- designs.js
+++ designs.js
@@ -1,10 +1,24 @@
+//Elements of the DOM
+
+const COLOR_PICKER = document.getElementById('colorPicker');
+const INPUT_HEIGHT = document.getElementById('inputHeight');
+const INPUT_WIDTH = document.getElementById('inputWidth');
+const SIZE_PICKER = document.getElementById('sizePicker');
+const PIXEL_CANVAS = document.getElementById('pixelCanvas');
+
 // Select color input
+
+
+
 // Select size input
+
+
 
 // When size is submitted by the user, call makeGrid()
 
+
+
 function makeGrid() {
 
-// Your code goes here!
 
 }
