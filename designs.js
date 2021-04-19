fac4efb25d0197593d71f5d3f09ed4bdf2b34277
Miguel Solano <miguel.abreu03@gmail.com>
2021-04-19 11:59:06 -0400

    feat: Adding auxiliary functions

--- designs.js
+++ designs.js
@@ -8,11 +8,27 @@ const PIXEL_CANVAS = document.getElementById('pixelCanvas');
 
 // Select color input
 
+function getColor() {    
+    let color = COLOR_PICKER.value;
+    return color;
+}
 
+function paintPixel(element) {
+    let color = getColor();
+    element.style = 'background-color: ' + color + ';';
+}
 
 // Select size input
 
+function getHeight() {
+    let height = INPUT_HEIGHT.value;
+    return height;
+}
 
+function getWidth() {
+    let width = INPUT_WIDTH.value;
+    return width;
+}
 
 // When size is submitted by the user, call makeGrid()
 
@@ -20,5 +36,5 @@ const PIXEL_CANVAS = document.getElementById('pixelCanvas');
 
 function makeGrid() {
 
-
+    }
 }
